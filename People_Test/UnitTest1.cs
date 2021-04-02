a1b75f6e35e5475f70ed2a7119abe2db73345a55
Алекс Архипов <todo123123@mail.ru>
2021-04-02 12:41:17 +0300

    Добавьте файлы проекта.

---
+++ People_Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ProjectTestExample;
+
+namespace People_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        People people = new People("gg", "1234", "gg@gmail.com", 70000000000);
+
+        [TestMethod]
+        public void GetNameTest()
+        {
+            Assert.AreEqual("gg", people.Login);
+            
+        }
+        [TestMethod]
+        public void GetEmailTest()
+        {
+            Assert.AreEqual("gg@gmail.com", people.Email);
+
+        }
+
+        [TestMethod]
+        public void GetPhoneTest()
+        {
+            Assert.AreEqual(70000000000, people.Phone);
+
+        }
+        [TestMethod]
+        public void GetPassTest()
+        {
+            Assert.AreEqual("1234", people.Pass);
+
+        }
+    }
+}
